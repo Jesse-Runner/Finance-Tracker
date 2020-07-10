7fda9d2c369771c10ae1409674d7584b78ff2df3
Jesse-Runner <jesseplusplus@gmail.com>
2020-07-10 14:01:47 -0400

    Added NavBar to front end

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ExpenseGroup from './ExpenseGroup';
+import Nav from './NavBar'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ExpenseGroup />
+    <Nav></Nav>
   </React.StrictMode>,
   document.getElementById('root')
 );
